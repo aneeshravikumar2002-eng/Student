538109adcd83a98aec4e4b8030525fffb2521f21
Aneesh Ravikumar <aneeshravikumar2002@gmail.com>
2026-05-15 05:18:57 +0000

    Edit login.cy.js

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -2,7 +2,7 @@ describe('Student Dashboard Test', () => {
 
   it('Login page should open', () => {
 
-    cy.visit('http://15.207.10.227:8000')
+    cy.visit('http://localhost:8000')
 
     cy.get('input[type="text"]')
       .type('aneesh')
