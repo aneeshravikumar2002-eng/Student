f0da070785301f2c8176d441cbec676a62c0d27f
aneesh <aneeshravikumar0@gmail.com>
2026-05-18 07:04:02 +0000

    Updated Coverage

---
+++ src/test/java/com/aneesh/studentdashboard/HomeControllerTest.java
@@ -0,0 +1,32 @@
+package com.aneesh.studentdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void homePageLoads() throws Exception {
+
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void dashboardPageLoads() throws Exception {
+
+        mockMvc.perform(get("/dashboard"))
+                .andExpect(status().isOk());
+    }
+}
